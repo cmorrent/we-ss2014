e1cb2bea221349b79edb90937480d58eab35bf42
Lukas Hubmaier <lukas@hubmaier.at>
2014-04-26 17:22:38 +0200

    Service added

---
+++ lab2/src/main/java/at/ac/tuwien/big/we14/lab2/api/service/AnswerService.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.big.we14.lab2.api.service;
+
+import java.util.List;
+
+/**
+ * Created by Lukas on 26.04.2014.
+ */
+public interface AnswerService {
+
+        public void answerQuestion(List<Integer> choices);
+
+}
---
+++ lab2/src/main/java/at/ac/tuwien/big/we14/lab2/api/service/RoundService.java
@@ -0,0 +1,15 @@
+package at.ac.tuwien.big.we14.lab2.api.service;
+
+/**
+ * Created by Lukas on 26.04.2014.
+ */
+public interface RoundService {
+
+    public void startRound();
+
+
+
+
+
+
+}
